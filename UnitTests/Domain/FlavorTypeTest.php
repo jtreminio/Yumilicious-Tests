6a58de19aa68640a137755cdc00e288917447e6f
Juan Treminio <jtreminio@gmail.com>
2012-10-16 15:43:18 -0500

    Covering ::create()

--- UnitTests/Domain/FlavorTypeTest.php
+++ UnitTests/Domain/FlavorTypeTest.php
@@ -55,4 +55,165 @@ class FlavorTypeTest extends Base
 
         $domainFlavorType->create($dataset);
     }
+
+    /**
+     * @test
+     * @covers \Yumilicious\Domain\FlavorType::create
+     */
+    public function createThrowsExceptionOnNameExists()
+    {
+        $this->setExpectedException(
+            'Yumilicious\Exception\Domain',
+            'Selected flavor type name is already in use'
+        );
+
+        $domainFlavorType = $this->getDomainFlavorType();
+        $daoFlavorType    = $this->getDaoFlavorType();
+
+        $dataset = array(
+            'parentId' => 123,
+            'name'     => 'test name'
+        );
+
+        $getOneByIdReturn = true;
+        $daoFlavorType->expects($this->once())
+            ->method('getOneById')
+            ->with($dataset['parentId'])
+            ->will($this->returnValue($getOneByIdReturn));
+
+        $getOneByNameReturn = true;
+        $daoFlavorType->expects($this->once())
+            ->method('getOneByName')
+            ->with($dataset['name'])
+            ->will($this->returnValue($getOneByNameReturn));
+
+        $this->setService('daoFlavorType', $daoFlavorType);
+
+        $domainFlavorType->create($dataset);
+    }
+
+    /**
+     * @test
+     * @covers \Yumilicious\Domain\FlavorType::create
+     */
+    public function createThrowsExceptionOnValidateFailure()
+    {
+        $this->setExpectedException(
+            'Yumilicious\Exception\Domain',
+            'updatedBy - This value should not be blank.<br />'
+        );
+
+        $domainFlavorType = $this->getDomainFlavorType();
+        $daoFlavorType    = $this->getDaoFlavorType();
+
+        $dataset = array(
+            'parentId' => 123,
+            'name'     => 'test name'
+        );
+
+        $getOneByIdReturn = true;
+        $daoFlavorType->expects($this->once())
+            ->method('getOneById')
+            ->with($dataset['parentId'])
+            ->will($this->returnValue($getOneByIdReturn));
+
+        $getOneByNameReturn = false;
+        $daoFlavorType->expects($this->once())
+            ->method('getOneByName')
+            ->with($dataset['name'])
+            ->will($this->returnValue($getOneByNameReturn));
+
+        $this->setService('daoFlavorType', $daoFlavorType);
+
+        $domainFlavorType->create($dataset);
+    }
+
+    /**
+     * @test
+     * @covers \Yumilicious\Domain\FlavorType::create
+     */
+    public function createReturnsFalseOnCreateFailure()
+    {
+        $domainFlavorType = $this->getDomainFlavorType();
+        $daoFlavorType    = $this->getDaoFlavorType();
+
+        $dataset = array(
+            'parentId' => 123,
+            'name'     => 'test name',
+            'updatedBy' => 321,
+        );
+
+        $getOneByIdReturn = true;
+        $daoFlavorType->expects($this->once())
+            ->method('getOneById')
+            ->with($dataset['parentId'])
+            ->will($this->returnValue($getOneByIdReturn));
+
+        $getOneByNameReturn = false;
+        $daoFlavorType->expects($this->once())
+            ->method('getOneByName')
+            ->with($dataset['name'])
+            ->will($this->returnValue($getOneByNameReturn));
+
+        $createReturn = false;
+        $daoFlavorType->expects($this->once())
+            ->method('create')
+            ->will($this->returnValue($createReturn));
+
+        $this->setService('daoFlavorType', $daoFlavorType);
+
+        $this->assertFalse(
+            $domainFlavorType->create($dataset),
+            'Expected ::create() to return false'
+        );
+    }
+
+    /**
+     * @test
+     * @covers \Yumilicious\Domain\FlavorType::create
+     */
+    public function createReturnsEntity()
+    {
+        $domainFlavorType = $this->getDomainFlavorType();
+        $daoFlavorType    = $this->getDaoFlavorType();
+
+        $dataset = array(
+            'parentId' => 123,
+            'name'     => 'test name',
+            'updatedBy' => 321,
+        );
+
+        $getOneByIdReturn = true;
+        $daoFlavorType->expects($this->once())
+            ->method('getOneById')
+            ->with($dataset['parentId'])
+            ->will($this->returnValue($getOneByIdReturn));
+
+        $getOneByNameReturn = false;
+        $daoFlavorType->expects($this->once())
+            ->method('getOneByName')
+            ->with($dataset['name'])
+            ->will($this->returnValue($getOneByNameReturn));
+
+        $createReturn = 456;
+        $daoFlavorType->expects($this->once())
+            ->method('create')
+            ->will($this->returnValue($createReturn));
+
+        $this->setService('daoFlavorType', $daoFlavorType);
+
+        $result = $domainFlavorType->create($dataset);
+
+        $this->assertEquals(
+            $createReturn,
+            $result->getId(),
+            'Expected entity getId to match'
+        );
+
+        $this->assertEquals(
+            $dataset['name'],
+            $result->getName(),
+            'Expected entity getId to match'
+        );
+    }
 }
